db8a8ecfc0523f5d0e9528ee8e3b932b3d3905dd
nikiteofanov <144799937+nikiteofanov@users.noreply.github.com>
2024-03-15 12:02:33 +0200

    Add project files.

---
+++ Chronometer/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Chronometer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.clockLabel1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.clockLabel2 = new System.Windows.Forms.Label();
+            this.clockLabel3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // clockLabel1
+            // 
+            this.clockLabel1.AutoSize = true;
+            this.clockLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clockLabel1.Location = new System.Drawing.Point(394, 228);
+            this.clockLabel1.Name = "clockLabel1";
+            this.clockLabel1.Size = new System.Drawing.Size(48, 32);
+            this.clockLabel1.TabIndex = 0;
+            this.clockLabel1.Text = "00";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(425, 361);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(576, 361);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "RESET";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // clockLabel2
+            // 
+            this.clockLabel2.AutoSize = true;
+            this.clockLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clockLabel2.Location = new System.Drawing.Point(445, 228);
+            this.clockLabel2.Name = "clockLabel2";
+            this.clockLabel2.Size = new System.Drawing.Size(48, 32);
+            this.clockLabel2.TabIndex = 4;
+            this.clockLabel2.Text = "00";
+            // 
+            // clockLabel3
+            // 
+            this.clockLabel3.AutoSize = true;
+            this.clockLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clockLabel3.Location = new System.Drawing.Point(497, 228);
+            this.clockLabel3.Name = "clockLabel3";
+            this.clockLabel3.Size = new System.Drawing.Size(65, 32);
+            this.clockLabel3.TabIndex = 5;
+            this.clockLabel3.Text = "000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(437, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(487, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(342, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 46);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Chronometer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(920, 583);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clockLabel3);
+            this.Controls.Add(this.clockLabel2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clockLabel1);
+            this.Name = "Form1";
+            this.Text = "NikolayTeofanov";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label clockLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label clockLabel2;
+        private System.Windows.Forms.Label clockLabel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Chronometer/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chronometer
+{
+    public partial class Form1 : Form
+    {
+        private int timeMs;
+        private int timeSec;
+        private int timeMin;
+        bool isActive;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetTime();
+            
+        }
+
+        private void ResetTime()
+        {
+            timeMs = 0;
+            timeSec = 0;
+            timeMin = 0;
+                
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isActive)
+            {
+                timeMs+=500;
+                if (timeMs >= 1000)
+                {
+                    timeSec++;
+                    timeMs = 0;
+
+                    if (timeSec >= 60)
+                    {
+                        timeMin++;
+                        timeSec = 0;
+                    }
+                }
+            }
+            
+            DrawTime();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isActive = true;
+            timer1.Start();
+        }
+
+        private void DrawTime()
+        {
+            clockLabel3.Text = String.Format("{0:000}", timeMs);
+            clockLabel2.Text = String.Format("{0:00}", timeSec);
+            clockLabel1.Text = String.Format("{0:00}", timeMin);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+            timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+            timer1.Stop();
+            ResetTime();
+            DrawTime();
+        }
+    }
+}
